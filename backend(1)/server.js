03213a18820f27b0f98c4b3059dc698599d04cc4
Ujala Chinchkhede <uajalachinchkhede@gmail.com>
2026-08-12 11:44:43 +0530

    Prepare Vendra for backend deployment

--- backend(1)/server.js
+++ backend(1)/server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 
+
 dotenv.config();
 connectDB();
 const app = express();
@@ -27,9 +28,10 @@ app.use('/api/orders', require("./routes/orderRoutes"));
 app.use('/api/payment', require("./routes/paymentRoutes"));
 app.use('/api/analytics', require("./routes/analyticsRoutes"));
 
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT ,() =>{
-    console.log(`Server is running on port ${PORT}`);
-});
 
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Open http://localhost:${PORT}`);
+});
